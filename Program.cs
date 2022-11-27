c45fdbdd40b4541268202bd2878d803c9d4fdffa
email <vassili642@gmail.com>
2022-11-27 16:49:45 +0300

    Add task 56

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@
 9 5 3 2
 8 4 4 2
 */
+/*
 Console.WriteLine("Input numbers of rows: ");
 int user_rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input numbers of columns: ");
@@ -68,4 +69,81 @@ void PrintArray(int[,] array)
     }
 }
 Console.WriteLine();
+*/
 
+/*
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+Console.WriteLine("Input numbers of rows: ");
+int user_rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input numbers of columns: ");
+int user_columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] array = new int[user_rows, user_columns];
+FillArrayRandomNumbers(array);
+PrintArray(array);
+
+int minSum = 0;
+int sum = SumLine(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSum = SumLine(array, i);
+  if (sum > tempSum)
+  {
+    sum = tempSum;
+    minSum = i;
+  }
+}
+
+Console.WriteLine($"\nString with the smallest sum ({sum}) of elements: {minSum+1}");
+
+
+int SumLine(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(" ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write(" ");
+        Console.WriteLine();
+    }
+}
+Console.WriteLine();
