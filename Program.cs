708976c2a69e4d1c7e94e0c3645733d1adc55f7f
email <vassili642@gmail.com>
2022-11-29 18:12:32 +0300

    Add task 58

--- Program.cs
+++ Program.cs
@@ -86,7 +86,7 @@ Console.WriteLine();
 
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 */
-
+/*
 Console.WriteLine("Input numbers of rows: ");
 int user_rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input numbers of columns: ");
@@ -120,6 +120,94 @@ int SumLine(int[,] array, int i)
   }
   return sumLine;
 }
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(" ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write(" ");
+        Console.WriteLine();
+    }
+}
+Console.WriteLine();
+*/
+
+/*
+Задача 58:(дополнительно) Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+Console.WriteLine("Input numbers of rows of first matrix: ");
+int user_rows1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input numbers of columns of first matrix: ");
+int user_columns1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input numbers of rows of second matrix: ");
+int user_rows2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input numbers of columns of second matrix: ");
+int user_columns2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if (user_rows1 != user_columns2)
+{
+    Console.WriteLine("The number of rows of the first matrix does not match the number of columns of the second matrix");
+    Console.WriteLine();
+    return;
+}
+ 
+int[,] firstMartrix = new int[user_rows1, user_columns1];
+FillArrayRandomNumbers(firstMartrix);
+Console.WriteLine($"\nFirst matrix:");
+PrintArray(firstMartrix);
+
+int[,] secondMartrix = new int[user_rows2, user_columns2];
+FillArrayRandomNumbers(secondMartrix);
+Console.WriteLine($"\nSecond martrix:");
+PrintArray(secondMartrix);
+
+int[,] resultMatrix = new int[user_rows1,user_columns2];
+
+MultiplyMatrix(firstMartrix, secondMartrix, resultMatrix);
+Console.WriteLine($"\nMultiplication of the first and second matrixes:");
+PrintArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
 
 void FillArrayRandomNumbers(int[,] array)
 {
