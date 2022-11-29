6cffe59067b99bf60ca31fee2bc1a4cfcd167bd2
email <vassili642@gmail.com>
2022-11-29 18:45:26 +0300

    Add task 60

--- Program.cs
+++ Program.cs
@@ -158,7 +158,7 @@ Console.WriteLine();
 18 20
 15 18
 */
-
+/*
 Console.WriteLine("Input numbers of rows of first matrix: ");
 int user_rows1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input numbers of columns of first matrix: ");
@@ -235,3 +235,82 @@ void PrintArray(int[,] array)
     }
 }
 Console.WriteLine();
+*/
+
+/*
+Задача 60.(дополнительно) ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+Console.WriteLine($"\nEnter the size of the 3D pattern:");
+int x = InputNumbers("Enter x: ");
+int y = InputNumbers("Enter y: ");
+int z = InputNumbers("Enter z: ");
+Console.WriteLine();
+
+int[,,] array3D = new int[x, y, z];
+CreateArray(array3D);
+PrintArray(array3D);
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void PrintArray (int[,,] array3D)
+{
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        Console.Write( $"{array3D[i,j,k]}({i},{j},{k}) ");
+      }
+      Console.WriteLine();
+    }
+    
+  }
+}
+Console.WriteLine();
+void CreateArray(int[,,] array3D)
+{
+  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < array3D.GetLength(0); x++)
+  {
+    for (int y = 0; y < array3D.GetLength(1); y++)
+    {
+      for (int z = 0; z < array3D.GetLength(2); z++)
+      {
+        array3D[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
